41c2585f57b971dd574f23b9e50d9dcb01c8b44a
liam2503 <liamceda@hotmail.com>
2026-03-13 17:58:21 +0900

    Fixed update error

--- release/update.bat
+++ release/update.bat
@@ -3,6 +3,6 @@ timeout /t 2 /nobreak > NUL
 xcopy /Y /E temp_update\Assets\* Assets\
 xcopy /Y temp_update\game.exe .\
 rmdir /S /Q temp_update
-del update.zip
+del game.zip
 start game.exe
 exit
--- release/update.sh
+++ release/update.sh
@@ -3,6 +3,6 @@ sleep 2
 cp -R temp_update/Assets/* game.app/Contents/Resources/Assets/
 cp temp_update/gameBin game.app/Contents/MacOS/
 rm -rf temp_update
-rm update.zip
+rm game.zip
 open game.app
 exit
